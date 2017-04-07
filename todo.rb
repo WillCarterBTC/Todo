7fdfaa68367a435773ec90740e29ccc3de7149f8
WillCarterBTC <willcarterbtc@gmail.com>
2017-04-07 09:53:36 +0700

    refactored print_array and prompt

--- todo.rb
+++ todo.rb
@@ -9,19 +9,16 @@ end
 def add(item)
   @arr << item
   #Time.now.utc
-  print_array(@arr)
 end
 
 def remove(item)
   @arr.delete_at(item.to_i - 1)
-  print_array(@arr)
 end
 
 def edit(item)
   puts 'What shall the new task be?'
   new_item = gets.strip
   @arr[@arr.index(item)] = new_item
-  print_array(@arr)
 end
 
 def prompt()
@@ -35,16 +32,14 @@ while item = gets.strip.downcase do
     if item.include? 'add'
     item.sub! 'add', ''
       add(item)
-      prompt
     elsif item.include? 'remove'
     item.sub! 'remove ', ''
       remove(item)
-      prompt
     elsif item.include? 'edit'
     item.sub! 'edit', ''
       edit(item)
-    prompt
     else puts 'ERROR Unknown command, try again.'
-    prompt
-end
+  end  
+  print_array(@arr)
+  prompt
 end
